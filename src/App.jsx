f7d8770884d16622dbe1b1b6e960d64aaeb16c7c
[pureeFoodFacilit] <jumael99@gmail.com>
2024-05-22 21:06:49 +0600

    added and learned prop validation

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   }, {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)}, {
     id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28),
   }, {
-    id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 5, 12),
+    id: 'e4', title: 12, amount: 450, date: new Date(2021, 5, 12),
   },];
 
   return (<div>
--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,10 +2,11 @@ import './ExpenseItem.css';
 import Card from '../UI/Card.jsx'
 import ExpenseDate from './ExpenseDate.jsx'
 import React, {useState} from 'react'
+import propTypes from 'prop-types'
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  
+
   const clickHandler = () => {
     setTitle('Updated!');
     console.log(title);
@@ -19,6 +20,12 @@ const ExpenseItem = (props) => {
     </div>
     <button onClick={clickHandler}>Change Title</button>
   </Card>);
+}
+
+ExpenseItem.propTypes = {
+  title: propTypes.string.isRequired,
+  amount: propTypes.number.isRequired,
+  date: propTypes.instanceOf(Date).isRequired
 }
 
 export default ExpenseItem;
